adb496d316e85acfd8cdaf384a8e7605f2caea57
tcLaptop <88120195+TcPirate1@users.noreply.github.com>
2024-07-31 12:58:37 +1200

    Set up base and home page

--- myProject/mha_website/models.py
+++ myProject/mha_website/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Card(models.Model):
+    def __init__(self, name, rarity, set, cardType, difficulty, control, blockZone, blockModifier, speed, attackZone, damage, symbols, cardText, keywords):
+        self.image = models.URLField()
+        self.name = name
+        self.rarity = rarity
+        self.set = set
+        self.cardType = cardType
+        self.difficulty = difficulty
+        self.control = control
+        self.blockZone = blockZone
+        self.blockModifier = blockModifier
+        self.speed = speed
+        self.attackZone = attackZone
+        self.damage = damage
+        self.symbols = symbols
+        self.cardText = cardText
+        self.keywords = keywords
+
+        def __str__(self):
+            return self.name
+
+class Deck(models.Model):
+    def __init__(self, name, by, main, main_card_copies, side_card_copies, total_cards, symbol, visibility, description):
+        user = models.ForeignKey("User", related_name=("decks"), on_delete=models.CASCADE)
+        self.name = name
+        self.by = by
+        self.main = main
+        self.main_card_copies = main_card_copies
+        self.side_card_copies = side_card_copies
+        self.total_cards = total_cards
+        self.symbol = symbol
+        self.visibility = visibility
+        self.description = description
+
+        def __str__(self):
+            return self.name
--- myProject/settings.py
+++ myProject/settings.py
@@ -27,6 +27,12 @@ DBname = os.getenv('database_name')
 client = MongoClient(MongodbConnectionString)
 db = client[DBname]
 
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+#     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+#     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+# }
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
