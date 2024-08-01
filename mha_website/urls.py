66ccffd621c77f707d8d98f0de5adf453d17b0b7
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2024-08-01 12:10:20 +1200

    Created signup, login and logout pages, created URL in home page.

--- mha_website/urls.py
+++ mha_website/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', homeView.as_view(), name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', name='login')),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/', name='logout'))
+    path('register/', registerView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- mha_website/views.py
+++ mha_website/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
+from django.contrib import messages
 
 # Create your views here.
 class homeView(TemplateView):
     template_name = "home.html"
+
+class registerView(View):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('home')
+        return render(request, self.template_name, {'form': form})
+    
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'registration/login.html'
+
+class CustomLogoutView(auth_views.LogoutView):
+    next_page = '/'
