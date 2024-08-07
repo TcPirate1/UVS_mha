baedbfb9fb01b44bfe8872d1b2b7c764a35632c0
tcLaptop <88120195+TcPirate1@users.noreply.github.com>
2024-08-07 16:18:26 +1200

    Added custom + bootstrap styling to form. Working on signup form.

--- mha_website/forms.py
+++ mha_website/forms.py
@@ -3,12 +3,12 @@ from django_recaptcha.fields import ReCaptchaV3
 
 class ContactForm(forms.Form):
     email = forms.EmailField(
-        widget=forms.TextInput(attrs={"placeholder": "Your e-mail"})
+        widget=forms.TextInput(attrs={"placeholder": "Your e-mail", "class": "form-control"})
     )
     subject = forms.CharField(
-        widget=forms.TextInput(attrs={"placeholder": "Subject"})
+        widget=forms.TextInput(attrs={"placeholder": "Subject", "class": "form-control"})
     )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={"placeholder": "Your message"})
+        widget=forms.Textarea(attrs={"placeholder": "Your message", "class": "form-control"})
     )
     captcha = ReCaptchaV3()
