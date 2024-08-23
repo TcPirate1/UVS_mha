17738456aafb83484bf6f8a44a3e1f6a65dc8d37
tcLaptop <88120195+TcPirate1@users.noreply.github.com>
2024-08-23 13:20:41 +1200

    Added UserPassesTestMixin to views. Tried setting up custom admin form. Getting "Not found: /admin/add_card" error.

--- mha_website/admin.py
+++ mha_website/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.http import HttpResponse
+from django.urls import path
+from .models import Card
 
 # Register your models here.
+class CardAdmin(admin.ModelAdmin):
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('add_card/', self.admin_site.admin_view(self.add_card_view))
+        ]
+        return custom_urls + urls
+
+    def add_card_view(self, request):
+        context = dict(
+            self.admin_site.each_context(request),
+            key = "value"
+        )
+        return TemplateResponse(request, "add_card.html", context)
+
+admin.site.register(Card, CardAdmin)
